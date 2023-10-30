c350d508940023541312e71a8423fde6d9038545
Jason Hu <jasonhu515@gmail.com>
2023-10-30 22:32:50 +0800

    front back interaction example plotly

---
+++ frontend_app_mini/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Plotly from 'plotly.js-dist';
+  import { onMount } from 'svelte';
+  
+  let plotData;
+  let layout;
+
+  onMount(async () => {
+    const res = await fetch('http://localhost:1234/getfakedata');
+    const { traces, layout: layoutConfig } = await res.json();
+    plotData = traces;
+    layout = layoutConfig;
+    Plotly.newPlot('myDiv', plotData, layout);
+  });
+</script>
+
+<div id="myDiv"></div>
--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # local import
 from llm_completion import (
@@ -9,10 +10,48 @@ from llm_completion import (
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080"  # Replace with the frontend server 
+]
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# @app.get("/")
+# async def read_root():
+#     return {"Hello": "World"}
+
+@app.get("/getfakedata")
+async def get_fake_data():
+    layout = {
+        "title": "Plot on [xxx]",
+        "xaxis": {"title": "Time"},
+        "yaxis": {"title": "Influencial events"},
+    }
+    traces = [
+        {
+            "x": [1, 2, 3, 4],
+            "y": [10, 11, 12, 13],
+            "type": "scatter",
+            "mode": "lines+markers",
+            "marker": {"size": [20, 40, 60, 80]},
+            "name": "Trace 1",
+        },
+        {
+            "x": [1, 2, 3, 4],
+            "y": [14, 15, 13, 16],
+            "type": "scatter",
+            "mode": "markers",
+            "name": "Trace 2",
+        },
+    ]
+    return {"traces": traces, "layout": layout}
 
 
 @app.get("/ask")
