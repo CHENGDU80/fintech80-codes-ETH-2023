d77aecfe9a538f1cad098c1823d288c76e9f7cbc
Jason Hu <jasonhu515@gmail.com>
2023-11-01 07:32:13 +0800

    try

--- server/main.py
+++ server/main.py
@@ -133,9 +133,10 @@ async def proc_news(
 
     lst_nc_news = []
     for doc in results:
-        doc['_id'] = str(doc['_id'])
-        nc_news = NCNews(**doc)
-        lst_nc_news.append(nc_news)
+        nc_news = NCNews.model_validate(doc)
+        # doc['_id'] = str(doc['_id'])
+        # nc_news = NCNews(**doc)
+        # lst_nc_news.append(nc_news)
 
     for nc_news in lst_nc_news:
         print(nc_news.query_str, nc_news.published_date)
